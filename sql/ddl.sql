942ce5a0b09933f543391c46c388ee460a7c955b
даша мамонтова <121117316+dmamontova@users.noreply.github.com>
2026-06-15 10:50:34 +0500

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,6 +1,7 @@
+--скрипт для создания справочника типов IoT-устройств
 DROP TABLE IF EXISTS device_types;
 
 CREATE TABLE device_types (
-    id INT PRIMARY KEY,
-    type_name VARCHAR(100) NOT NULL
+    id INT PRIMARY KEY, --идентификатор устройства
+    type_name VARCHAR(100) NOT NULL --тип устройства
 );
