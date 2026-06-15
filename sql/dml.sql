063f7912405c8b5cdddbef4370cbc622d9c84b14
даша мамонтова <121117316+dmamontova@users.noreply.github.com>
2026-06-15 10:53:13 +0500

    Update dml.sql

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,6 +1,7 @@
+--скрипт для наполнения данными справочника IoT-устройств тестовыми значениями
 INSERT INTO device_types (id, type_name) VALUES
-(1, 'temperature_sensor'),
-(2, 'humidity_sensor'),
-(3, 'weather_station'),
-(4, 'smart_thermostat'),
-(5, 'air_quality_sensor');
+(1, 'temperature_sensor'), --датчик температуры
+(2, 'humidity_sensor'), --датчик влажности
+(3, 'weather_station'), --погодная станция
+(4, 'smart_thermostat'), --термостат
+(5, 'air_quality_sensor'); --датчик качества воздуха
